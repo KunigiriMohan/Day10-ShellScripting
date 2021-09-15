d261f314406955b80770c11c10edf9fd147cb6a4
KunigiriMohan <90668633+KunigiriMohan@users.noreply.github.com>
2021-09-15 19:01:23 +0530

    Update flipCoinCombination.sh

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -6,25 +6,13 @@ do
 coin[i]=$((RANDOM%2))
 coin2[i]=$((RANDOM%2))
 coin3[i]=$((RANDOM%2))
-      if [[ ${coin[i]} -eq 0 ]]
+      if [[ ${coin[i]} -eq 1 && ${coin2[i]} -eq 1 && ${coin3[i]} -eq 1 ]]
       then
          count=$(($count+1))
+         flip[i]="HHH"
       else
          count2=$(($count2+1))
-      fi
-
-      if [[ ${coin2[i]} -eq 0 ]]
-      then
-         count=$(($count+1))
-      else
-         count2=$(($count2+1))
-      fi
-
-      if [[ ${coin3[i]} -eq 0 ]]
-      then
-         count=$(($count+1))
-      else
-         count2=$(($count2+1))
+         flip[i]="TTT"
       fi
 
 
@@ -38,10 +26,5 @@ echo "percentage of Heads is "$percentage_of_heads
 echo "percentage of tails is "$percentage_of_tails
 
 
-Dictionary[Triplet1]=${coin[@]}
-Dictionary[Triplet2]=${coin2[@]}
-Dictionary[Triplet3]=${coin3[@]}
-
-
-
-
+Dictionary[Triplet]=${flip[@]}
+echo ${Dictionary[@]}
